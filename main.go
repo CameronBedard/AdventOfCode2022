1bce63de1a65d26ab00f5bf8ff2ef1d96dc5dd70
Cameron Bedard <cam.bedardca@gmail.com>
2023-12-09 09:48:58 -0500

    day 9

--- main.go
+++ main.go
@@ -3,8 +3,25 @@ package main
 import (
 	//"AOC/solutions2022"
 	"AOC/solutions2023"
+	"bufio"
+	"fmt"
+	"os"
 )
 
 func main() {
-	solutions2023.Day7p2()
+	file, err := os.Open("/Users/cameron.bedard/Documents/FunRepos/AdventOfCode2022/inputs2023/day9.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	lines := make([]string, 0)
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	solutions2023.Day9p2(lines)
 }
---
+++ solutions2023/day9-1.go
@@ -0,0 +1,51 @@
+package solutions2023
+
+import (
+	"fmt"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func Day9p1(lines []string) {
+	entries := make([][]int, len(lines))
+
+	for i := range lines {
+		tmp := strings.Split(lines[i], " ")
+		line := make([]int, len(tmp))
+
+		for j := range tmp {
+			x, _ := strconv.Atoi(tmp[j])
+			line[j] = x
+		}
+		entries[i] = line
+	}
+
+	total := 0
+	for hist := range entries {
+		diffs := make([][]int, 1)
+		diffs[0] = entries[hist]
+
+		//while our top row isnt zeroed
+		for i := 0; !slices.Equal(diffs[i], make([]int, len(diffs[i]))); i++ {
+			//make new arr depth i+1 with -1 length
+			diffs = append(diffs, make([]int, len(diffs[i])-1))
+
+			for j := range diffs[i+1] {
+				diffs[i+1][j] = diffs[i][j+1] - diffs[i][j]
+			}
+		}
+		//bottom row of diffs is now 0's
+		degree := len(diffs) - 1
+		diffs[degree] = append(diffs[degree], 0)
+
+		for depth := degree - 1; depth >= 0; depth-- {
+			size := len(diffs[depth]) - 1
+			diffs[depth] = append(diffs[depth], diffs[depth][size]+diffs[depth+1][size])
+		}
+
+		total += diffs[0][len(diffs[0])-1]
+	}
+
+	fmt.Println("total", total)
+}
